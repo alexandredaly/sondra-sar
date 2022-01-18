bc9da35df512735f9b67b6dcf5577dc8842500fc
alexandre.daly <alexandre.daly@student-cs.fr>
2022-01-19 00:42:51 +0100

    [*] Fix bug dataset path

--- SwinIR_train.py
+++ SwinIR_train.py
@@ -4,7 +4,7 @@ import argparse
 import torch                                                  
 import torchvision.transforms as transforms
 from data.SARdataset import SARdataset
-from data .dataset_transformer import DatasetTransformer
+from data.dataset_transformer import DatasetTransformer
 
 # Create datasets
 def create_dataset(cfg):
@@ -15,7 +15,7 @@ def create_dataset(cfg):
     train_valid_dataset = SARdataset(os.path.join('data/', cfg['TRAIN_DATA_DIR'][2:]))
 
     # Split it into training and validation sets
-    nb_train = int((1.0 - valid_ratio) * len(train_valid_dataset)) + 1
+    nb_train = int((1.0 - valid_ratio) * len(train_valid_dataset))
     nb_valid =  int(valid_ratio * len(train_valid_dataset))
     train_dataset, valid_dataset = torch.utils.data.dataset.random_split(train_valid_dataset, [nb_train, nb_valid])
     
@@ -23,25 +23,6 @@ def create_dataset(cfg):
     train_dataset = DatasetTransformer(train_dataset, transforms.ToTensor())
     valid_dataset = DatasetTransformer(valid_dataset, transforms.ToTensor())
     return train_dataset, valid_dataset
-
-def create_dataloader(cfg, train_dataset, valid_dataset):
-    # Loading the batch size and the number of CPU threads from the config.yaml file
-    num_threads = cfg['DATASET']['NUM_THREADS'] 
-    batch_size  = cfg['DATASET']['BATCH_SIZE']
-
-    # Define the dataloaders for the train and validation sets
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                            batch_size=batch_size,
-                                            shuffle=True,                # <-- this reshuffles the data at every epoch
-                                            num_workers=num_threads)
-
-    valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset,
-                                            batch_size=batch_size, 
-                                            shuffle=False,
-                                            num_workers=num_threads)
-
-    return train_loader, valid_loader
-
 
 
 if __name__=="__main__":
@@ -64,13 +45,4 @@ if __name__=="__main__":
 
     # Build the datasets
     train_dataset, valid_dataset = create_dataset(cfg)
-
-    # Build the dataloaders
-    train_loader, valid_loader = create_dataloader(cfg, train_dataset, valid_dataset)
-
-    # Print informations about the dataset size and number of batches
-    print("The train set contains {} images, in {} batches".format(len(train_loader.dataset), len(train_loader)))
-    print("The validation set contains {} images, in {} batches".format(len(valid_loader.dataset), len(valid_loader)))
-
-    # 
 
