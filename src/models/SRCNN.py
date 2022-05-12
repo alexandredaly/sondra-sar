cc41ebf4035cce513d46881b979848055adaeb62
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-05-12 14:38:16 +0200

    preserver the corner values in upsampling

--- src/models/SRCNN.py
+++ src/models/SRCNN.py
@@ -4,7 +4,7 @@ from torch import nn
 class SRCNN(nn.Module):
     def __init__(self, num_channels=1):
         super(SRCNN, self).__init__()
-        self.upsample = nn.Upsample(scale_factor=2, mode="bicubic")
+        self.upsample = nn.Upsample(scale_factor=2, mode="bicubic", align_corners=True)
         self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
         self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
         self.conv3 = nn.Conv2d(32, num_channels, kernel_size=5, padding=5 // 2)
