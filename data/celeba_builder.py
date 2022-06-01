fe7ec53c36856c44393c6efa98433176a93aaca8
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-06-01 19:25:05 +0200

    dup

--- data/celeba_builder.py
+++ data/celeba_builder.py
@@ -3,11 +3,29 @@
 import pathlib
 import argparse
 import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+
+# def process_image(srcpath, dstpath):
+
+IMG_HEIGHT = 218
+IMG_WIDTH = 178
 
 
 def main(args):
-    dataset = torchvision.datasets.CelebA(str(args.rootdir))
+    dataset = torchvision.datasets.CelebA(
+        str(args.rootdir), transform=transforms.Compose([transforms.Grayscale()])
+    )
+
+    highres_to_lowres = transforms.Compose(
+        [transforms.Resize(size=(IMG_HEIGHT // 2, IMG_WIDTH // 2))]
+    )
+
     print(f"I loaded {len(dataset)} samples")
+    for highres, metadata in dataset:
+        lowres = highres_to_lowres(highres)
+        print(highres.size, lowres.size)
 
 
 if __name__ == "__main__":
