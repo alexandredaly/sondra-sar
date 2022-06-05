fa6d626ec077745b1bb151c8999ce0d763d4da07
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-06-05 11:59:59 +0200

    upd

--- job.py
+++ job.py
@@ -116,15 +116,14 @@ def generate_job(loss, model):
         content = content.replace("@SAVE_MODEL_DIR@", str(SAVE_MODEL_DIR))
         content = content.replace("@BATCH_SIZE@", str(batch_size))
         content = content.replace("@LOSS@", str(loss))
-        content = content.replace("@MODEL@", str(model))
-    print(content)
+        # content = content.replace("@MODEL@", str(model))
     print(f"Writting into {config_path} for {loss} and {model}")
     with open(config_path, "w") as f:
         f.write(content)
     print(f"Config file saved as {config_path}")
 
     # Launch the batch jobs
-    # submit_job(makejob(commit_id, config_path))
+    submit_job(makejob(commit_id, config_path))
 
 
 for model in ["SRCNN", "SRCNN2", "PixelShuffle", "SwinTransformer"]:
