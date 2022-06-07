6c563c3af1ca3df2e4d0118fb59c638a16db26c8
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-06-07 22:21:22 +0200

    run only on sh

--- job.py
+++ job.py
@@ -13,12 +13,12 @@ def makejob(commit_id, config_path):
     return f"""#!/bin/bash 
 
 #SBATCH --job-name=super-SAR
-#SBATCH --nodes=1
+#SBATCH --nodes=11
 #SBATCH --partition=gpu_inter
 #SBATCH --time=2:00:00
 #SBATCH --output=logslurms/slurm-%j.out
 #SBATCH --error=logslurms/slurm-%j.err
-#SBATCH --exclude=sh00,sh[10-16],cam[00-16]
+#SBATCH --exclude=sh00,sh[10-16],cam[00-16],tx[00-16]
 
 # Load the conda module
 export PATH=/opt/conda/bin:$PATH
