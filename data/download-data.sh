d1418814f1b7b314deefee022a20fd0c546652c9
Jeremy Fix <jeremy.fix@centralesupelec.fr>
2022-06-08 19:43:02 +0200

    put infer data in its own dir

--- data/download-data.sh
+++ data/download-data.sh
@@ -9,7 +9,7 @@ wget
 https://downloaduav2.jpl.nasa.gov/Release29/SSurge_12600_01/SSurge_12600_14170_008_141120_L090HH_01_BC_s2_1x1.slc.png -P ./data_files/raw_data
 
 
-wget https://downloaduav2.jpl.nasa.gov/Release29/SSurge_15305_01/SSurge_15305_14170_007_141120_L090HH_01_BC.ann -P ./data_files/raw_data
-wget https://downloaduav2.jpl.nasa.gov/Release29/SSurge_15305_01/SSurge_15305_14170_007_141120_L090HH_01_BC_s1_1x1.slc -P ./data_files/raw_data
+wget https://downloaduav2.jpl.nasa.gov/Release29/SSurge_15305_01/SSurge_15305_14170_007_141120_L090HH_01_BC.ann -P ./data_files/infer_raw_data
+wget https://downloaduav2.jpl.nasa.gov/Release29/SSurge_15305_01/SSurge_15305_14170_007_141120_L090HH_01_BC_s1_1x1.slc -P ./data_files/infer_raw_data
 wget
-https://downloaduav2.jpl.nasa.gov/Release29/SSurge_15305_01/SSurge_15305_14170_007_141120_L090HH_01_BC_s1_1x1.slc.png -P ./data_files/raw_data
+https://downloaduav2.jpl.nasa.gov/Release29/SSurge_15305_01/SSurge_15305_14170_007_141120_L090HH_01_BC_s1_1x1.slc.png -P ./data_files/infer_raw_data
